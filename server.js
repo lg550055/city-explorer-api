3503a344593d58fb7d55ea9e7682c4d11709cb6a
polo <lg550055@hotmail.com>
2022-01-19 15:57:20 -0600

    Adds live weather forecast by lat and long

--- server.js
+++ server.js
@@ -12,25 +12,28 @@ const PORT = process.env.PORT || 3002;
 const cors = require('cors');
 app.use(cors());
 
-// read data from weather.json
-const weatherData = require('./data/weather.json');
+const axios = require('axios');
 
+// Routes
 app.get('/', (req, res) => {
   res.send('Serves says Hello!');
 });
 
-// API endpoint
-app.get('/weather', (req, res) => {
-  let city = req.query.city.toLowerCase();
+// weather API endpoint
+app.get('/weather', async (req, res) => {
+  let lat = req.query.lat;
+  let lon = req.query.lat;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=7&key=${process.env.WEATHER_KEY}`;
   try {
-    let targetCity = weatherData.find(e => e.city_name.toLowerCase() === city);
-    let weatherForecast = targetCity.data.map(e => new Forecast(e));
+    let rawInfo = await axios.get(url);
+    let weatherForecast = rawInfo.data.data.map(e => new Forecast(e));
     res.send(weatherForecast);
   } catch (error) {
-    res.send(`There is no info on ${city}. Try Seattle, Paris or Amman`);
+    res.status(400).send(error.code);
   }
 });
 
+// catch all route
 app.get('*', (req, res) => {
   res.status(404).send('Endpoint not found, please check your intended url');
 });
