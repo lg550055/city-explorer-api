7595af1a9be614e9c8023243d84d059862e08f58
polo <lg550055@hotmail.com>
2022-01-18 22:04:42 -0600

    Adds backend functionality

--- server.js
+++ server.js
@@ -19,9 +19,26 @@ app.get('/', (req, res) => {
 
 // API endpoint
 app.get('/weather', (req, res) => {
-  let city = req.query.city;
-  console.log(city);
+  let city = req.query.city.toLowerCase();
+  try {
+    let targetCity = weatherData.filter(e => e.city_name.toLowerCase() === city);
+    let weatherForecast = targetCity[0].data.map(e => new Forecast(e));
+    res.send(weatherForecast);
+  } catch (error) {
+    res.send(`There is no info on ${city}. Try Seattle, Paris or Amman`);
+  }
 });
+
+app.get('*', (req, res) => {
+  res.status(404).send('Endpoint not found, please check your intended url');
+});
+
+class Forecast {
+  constructor(city) {
+    this.date = city.datetime;
+    this.description = city.weather.description;
+  }
+}
 
 // instruct server to start listening for requests
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
